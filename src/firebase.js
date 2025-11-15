6d92cf6aadc6f83e7498e94c6ba5bb419261bbee
Sqmu <magicesgo@gmail.com>
2025-11-15 15:11:43 -0500

    Refactor Firebase configuration to use environment variables; update .gitignore to include .env file and remove unused Cloudinary file.

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAOjvz1K0_RAZwb1BKqRGjrDFKOFArCi8A",
-  authDomain: "lifestyle-46a11.firebaseapp.com",
-  projectId: "lifestyle-46a11",
-  storageBucket: "lifestyle-46a11.firebasestorage.app",
-  messagingSenderId: "853178107062",
-  appId: "1:853178107062:web:d9c88ecb3d8066be92d217"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
